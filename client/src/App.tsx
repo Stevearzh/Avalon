1c4172c61512cb99bc19b74b829dac65ad82a857
Stevearzh <zy911004@gmail.com>
2017-12-10 00:07:08 +0800

    server can be used now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react';
+import * as moment from 'moment';
+
+import NavBar from './components/NavBar';
+import LogContent from './components/LogContent';
 
 import './App.css';
-import NavBar from './components/NavBar';
 
-class App extends React.Component {
+interface Props {}
+
+interface State {
+  selectedDate: moment.Moment;
+  selectedChannel: string;
+}
+
+class App extends React.Component<Props, State> {
+  state = {
+    selectedDate: moment(),
+    selectedChannel: ''
+  };
+
+  handleDateChange = (newDate: moment.Moment): void =>
+    this.setState({ selectedDate: newDate }) 
+
+  handleChannelChange = (newChannel: string): void =>
+    this.setState({ selectedChannel: newChannel })
+
   render() {
     return (
       <div className="App">
-        <NavBar/>
+        <NavBar
+          onDateChange={this.handleDateChange}
+          onChannelChange={this.handleChannelChange}
+          selectedChannel={this.state.selectedChannel}
+          selectedDate={this.state.selectedDate}
+        />
+        <LogContent
+          selectedChannel={this.state.selectedChannel}
+          selectedDate={this.state.selectedDate}
+        />
       </div>
     );
   }
