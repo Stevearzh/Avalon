530e1f65d3f6d776586bcbc639e703e15b60b6da
yu.zhang <yu.zhang@chaitin.com>
2019-04-04 16:14:47 +0800

    re-generate client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,45 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
-import './App.css';
 
+import './App.css';
 import logo from './logo.svg';
 
-class App extends React.Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+export interface Props extends WithStyles<typeof styles> {}
+
+class App extends React.Component<Props> {
   public render() {
+    const { classes } = this.props;
+
     return (
       <div className="App">
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                Material UI
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,9 +47,10 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="contained" color="primary">Hello, world!</Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
