95a830adc5e4eec5ed75b9eb4996cf821bdbf185
Stevearzh <zy911004@gmail.com>
2019-04-22 22:55:28 +0800

    move channel select to drawer

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
 import { StyleRules, withStyles, WithStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import Menu from '@material-ui/icons/Menu';
-import ModeComment from '@material-ui/icons/ModeComment';
 import { DatePicker, MaterialUiPickersDate } from 'material-ui-pickers';
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -15,6 +16,7 @@ import { bindActionCreators } from 'redux';
 
 import { actionCreators as channelActionCreators, State as Channel } from '@models/channel';
 import { actionCreators as dateActionCreators, State as DateState } from '@models/date';
+import { DRAWER_WIDTH } from '@src/const';
 import { Dispatch, RootState } from '@src/models';
 
 const styles: StyleRules = {
@@ -35,29 +37,6 @@ const styles: StyleRules = {
     top: '1em',
     right: '2em',
   },
-  'select-bar': {
-    width: '8em',
-    color: 'white',
-    marginRight: '1.5em',
-    marginLeft: '0.5em',
-
-    '&:before': {
-      borderColor: 'white',
-    },
-
-    '&:hover:before': {
-      borderColor: 'white !important',
-    },
-
-    '& svg': {
-      color: 'white !important',
-      marginTop: '-3px',
-    },
-
-    '& > div > div': {
-      marginTop: '-6px',
-    },
-  },
   'date-picker': {
     width: '8em',
     marginLeft: '0.5em',
@@ -74,9 +53,22 @@ const styles: StyleRules = {
       color: 'white',
     },
   },
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    justifyContent: 'flex-end',
+  },
 };
 
 interface Props extends WithStyles<StyleRules> {}
+
+interface State {
+  drawerOpen: boolean;
+}
 
 interface StateProps {
   channel: Channel;
@@ -102,14 +94,23 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
     dispatch,
   );
 
-class NavBar extends React.Component<Props & StateProps & DispatchProps> {
-  private handleChannelChange = (event: React.ChangeEvent<HTMLSelectElement>) =>
-    this.props.selectChannel(event.target.value); // tslint:disable-line:semicolon
+class NavBar extends React.Component<Props & StateProps & DispatchProps, State> {
+  public state: State = { drawerOpen: false };
 
   private handleDateChange = (date: MaterialUiPickersDate) => this.props.changeCurrentDate(date); // tslint:disable-line:semicolon
 
+  private handleDrawerOpen = () => this.setState({ drawerOpen: true });
+
+  private handleDrawerClose = () => this.setState({ drawerOpen: false });
+
+  private handleChannelSelect = (channel: string) => () => {
+    this.props.selectChannel(channel);
+    this.handleDrawerClose();
+  }; // tslint:disable-line:semicolon
+
   public render() {
     const { classes, channel, date } = this.props;
+    const { drawerOpen } = this.state;
 
     const channelList = (channel && channel.list) || [];
 
@@ -122,26 +123,18 @@ class NavBar extends React.Component<Props & StateProps & DispatchProps> {
       <div className={classes['nav-bar']}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton className={classes['menu-button']} color="inherit" aria-label="Menu">
+            <IconButton
+              className={classes['menu-button']}
+              color="inherit"
+              aria-label="Menu"
+              onClick={this.handleDrawerOpen}
+            >
               <Menu />
             </IconButton>
             <Typography variant="h6" color="inherit">
-              IRC Logs
+              {channel.choosen} Logs
             </Typography>
             <Typography variant="h6" color="inherit" className={classes['nav-items']}>
-              <ModeComment />
-              <Select
-                value={(channel && channel.choosen) || ''}
-                onChange={this.handleChannelChange}
-                inputProps={{ name: 'channel', id: 'channel-select' }}
-                className={classes['select-bar']}
-              >
-                {channelList.map((ch: string, i: number) => (
-                  <MenuItem value={ch} key={i}>
-                    <em>{ch}</em>
-                  </MenuItem>
-                ))}
-              </Select>
               <CalendarToday />
               <DatePicker
                 className={classes['date-picker']}
@@ -155,6 +148,28 @@ class NavBar extends React.Component<Props & StateProps & DispatchProps> {
             </Typography>
           </Toolbar>
         </AppBar>
+        <Drawer
+          variant="persistent"
+          anchor="left"
+          open={drawerOpen}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeft />
+            </IconButton>
+          </div>
+          <Divider />
+          {channelList.map((ch: string, i: number) => (
+            <div key={i} onClick={channel.choosen === ch ? () => null : this.handleChannelSelect(ch)}>
+              <MenuItem value={ch} selected={channel.choosen === ch}>
+                <em>{ch}</em>
+              </MenuItem>
+            </div>
+          ))}
+        </Drawer>
       </div>
     );
   }
--- client/src/const.ts
+++ client/src/const.ts
@@ -4,4 +4,6 @@ export const DEFAULT_PAGE_SIZE = 20;
 
 export const DATE_FORMATER = 'yyyy-MM-dd';
 
-export const PAGE_SIZE_SELECTIONS = [DEFAULT_PAGE_SIZE, DEFAULT_PAGE_SIZE * 2.5, DEFAULT_PAGE_SIZE * 5];
+export const PAGE_SIZE_SELECTIONS = [20, 50, 100];
+
+export const DRAWER_WIDTH = 240;
